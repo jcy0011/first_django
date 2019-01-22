9eeaadf3907a27fd03eb2a18168ea1b92ca0ec9b
jcy0011 <jcy0011@yonsei.ac.kr>
2019-01-23 04:35:31 +0900

    4 patterns of CBV

--- dojo/urls.py
+++ dojo/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from . import views_cbv
+
 urlpatterns=[
     url(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
     url(r'^hello/(?P<name>[ㄱ-ㅎㅏ-ㅣ가-힣]{2,3})/(?P<age>\d+)/$', views.hello),
@@ -7,4 +9,9 @@ urlpatterns=[
     url(r'^list2/$', views.post_list2),
     url(r'^list3/$', views.post_list3),
     url(r'^excel/$', views.excel_download),
+
+    url(r'^cbvlist1/$', views_cbv.post_list1),
+    url(r'^cbvlist2/$', views_cbv.post_list2),
+    url(r'^cbvlist3/$', views_cbv.post_list3),
+    url(r'^cbvexcel/$', views_cbv.excel_download),
 ]
