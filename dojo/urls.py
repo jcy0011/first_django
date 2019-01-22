3d0103f32864e2b67303637058ad4672f76fac7c
jcy0011 <jcy0011@yonsei.ac.kr>
2019-01-23 03:59:04 +0900

    4 patterns of FBV

--- dojo/urls.py
+++ dojo/urls.py
@@ -3,4 +3,8 @@ from . import views
 urlpatterns=[
     url(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
     url(r'^hello/(?P<name>[ㄱ-ㅎㅏ-ㅣ가-힣]{2,3})/(?P<age>\d+)/$', views.hello),
+    url(r'^list1/$', views.post_list1),
+    url(r'^list2/$', views.post_list2),
+    url(r'^list3/$', views.post_list3),
+    url(r'^excel/$', views.excel_download),
 ]
--- dojo/views.py
+++ dojo/views.py
@@ -1,5 +1,7 @@
+import os
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.conf import settings
 # Create your views here.
 def mysum(request, numbers):
     # request: HttpRequest
@@ -8,3 +10,29 @@ def mysum(request, numbers):
 
 def hello(request, name, age):
     return HttpResponse('안녕하세요. {}. {}살이시네요.'.format(name,age))
+
+def post_list1(request):
+    name = 'j'
+    return HttpResponse('''
+    <h1>AskDjango</h1>
+    <p>{name}</p>
+    <p>여러분의 파이선 장고 페이스메이커가 되겠습니다.</p>
+    '''.format(name=name))
+
+def post_list2(request):
+    name = 'jay'
+    return render(request, 'dojo/post_list.html', {'name': name})
+
+def post_list3(request):
+    return JsonResponse({
+        'message': '안녕 파이썬&장고',
+        'items' : ['파이썬', '장고', 'Celery', 'Azure', 'AWS']
+    }, json_dumps_params={'ensure_ascii': False})
+
+def excel_download(request):
+    filepath = os.path.join(settings.BASE_DIR, 'test_import.xlsx')
+    filename = os.path.basename(filepath)
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type = 'application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+        return response
