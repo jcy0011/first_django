3046dc3ab2ea829953a331393341aca72a95eac9
jcy0011 <jcy0011@yonsei.ac.kr>
2019-01-23 02:27:11 +0900

    dojo/views/mysum takes three arguments

--- dojo/urls.py
+++ dojo/urls.py
@@ -1,5 +1,7 @@
-from django.conf.urls import urls
-
+from django.conf.urls import url
+from . import views
 urlpatterns=[
-
+    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum),
+    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', views.mysum),
+    url(r'^sum/(?P<x>\d+)/$', views.mysum),
 ]
