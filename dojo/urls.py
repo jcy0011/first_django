0cf5a4f848415f6b3c5ae012ee3fb35ff29808f6
jcy0011 <jcy0011@yonsei.ac.kr>
2019-01-23 02:43:51 +0900

    dojo.views.hello can take any korean letters of 2 or 3 syllables as name and any digit as age

--- dojo/urls.py
+++ dojo/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from . import views
 urlpatterns=[
     url(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
+    url(r'^hello/(?P<name>[ㄱ-ㅎㅏ-ㅣ가-힣]{2,3})/(?P<age>\d+)/$', views.hello),
 ]
