c5f67f3a865318ee6f4be0b0fe7172b4ee6696c2
jcy0011 <jcy0011@yonsei.ac.kr>
2019-01-23 06:52:48 +0900

    list_display cust

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import Post
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'created_at', 'updated_at']
-
+    list_display = ['id', 'title', 'content_size', 'created_at', 'updated_at']
+    
+    def content_size(self, post):
+        return mark_safe('<strong>{}</strong> letters/whitespaces'.format(len(post.content)))
+    content_size.short_description = 'letter counts'
 #admin.site.register(Post)
