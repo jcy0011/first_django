8c9f8d89e2e4efabb0fab592b558501652ef194d
jcy0011 <jcy0011@yonsei.ac.kr>
2019-01-23 07:12:44 +0900

    admin actions added

--- blog/admin.py
+++ blog/admin.py
@@ -5,9 +5,26 @@ from django.utils.safestring import mark_safe
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'content_size', 'created_at', 'updated_at']
-    
+    list_display = ['id', 'title', 'content_size', 'status', 'created_at', 'updated_at',]
+
+    actions = ['make_draft', 'make_published', 'make_withdrawn',]
+
     def content_size(self, post):
         return mark_safe('<strong>{}</strong> letters/whitespaces'.format(len(post.content)))
     content_size.short_description = 'letter counts'
-#admin.site.register(Post)
+    #admin.site.register(Post)
+
+    def make_published(self, request, queryset):
+        updated_count = queryset.update(status='p')
+        self.message_user(request, 'Changed the status of {} posts to "Published" status.'.format(updated_count))
+    make_published.short_description = "Change status to 'Published'"
+
+    def make_draft(self, request, queryset):
+        updated_count = queryset.update(status='d')
+        self.message_user(request, 'Changed the status of {} posts to "Draft" status.'.format(updated_count))
+    make_draft.short_description = "Change status to 'Draft'"
+
+    def make_withdrawn(self, request, queryset):
+        updated_count = queryset.update(status='w')
+        self.message_user(request, 'Changed the status of {} posts to "Withdrawn" status.'.format(updated_count))
+    make_withdrawn.short_description = "Change status to 'Withdrawn'"
--- blog/models.py
+++ blog/models.py
@@ -8,10 +8,17 @@ def lnglat_validator(value):
         raise ValidationError('Invalid  Lng/Lat Type')
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published'),
+        ('w', 'Withdrawn'),
+    )
+
     author = models.CharField(max_length=20)
     title = models.CharField(max_length=100, verbose_name="Article Headline", help_text="Write the title shorter than 100 letters/whitespaces.")
     content = models.TextField(verbose_name="Article Content")
     tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50, blank=True, validators=[lnglat_validator], help_text='Write in the format of longitude and latitude.')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
