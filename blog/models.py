1596cf14466007d145dbacfd5e20b520059f9ca8
jcy0011 <jcy0011@yonsei.ac.kr>
2019-01-23 05:40:16 +0900

    optional fields added (tag and long-, latitude)

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+import re
+from django.forms import ValidationError
 
 # Create your models here.
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+        raise ValidationError('Invalid  Lng/Lat Type')
+
 class Post(models.Model):
-    title = models.CharField(max_length=100, verbose_name="Article Headline", help_text="Write the title shorter than 100 letters/whitespaces")
+    title = models.CharField(max_length=100, verbose_name="Article Headline", help_text="Write the title shorter than 100 letters/whitespaces.")
     content = models.TextField(verbose_name="Article Content")
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50, blank=True, validators=[lnglat_validator], help_text='Write in the format of longitude and latitude.')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
