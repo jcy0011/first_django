ec1529eb712a3f875dad826bc255610acae99e32
jcy0011 <jcy0011@yonsei.ac.kr>
2019-01-23 02:09:29 +0900

    dojo app added and shop app deleted

--- first_django/urls.py
+++ first_django/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, re_path
 from django.conf.urls import include, url
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls'))
+    url(r'^blog/', include('blog.urls')),
+    url(r'^dojo/', include('dojo.urls')),
 ]
