9ac912dd5a024a11a6b61e5538188c9c04eb6783
jcy0011 <jcy0011@yonsei.ac.kr>
2019-01-23 05:27:59 +0900

    blog.models.Post model created and migrated

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Article Headline", help_text="Write the title shorter than 100 letters/whitespaces")
+    content = models.TextField(verbose_name="Article Content")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
