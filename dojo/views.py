99d0d02b81b24c09ac6a07289862810b41c5b56d
jcy0011 <jcy0011@yonsei.ac.kr>
2019-01-23 02:35:03 +0900

    dojo.views.mysum can take any numbers or numeric args

--- dojo/views.py
+++ dojo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def mysum(request, x, y=0, z=0):
+def mysum(request, numbers):
     # request: HttpRequest
-    return HttpResponse(int(x)+int(y)+int(z))
+    result = sum(map(lambda s: int(s or 0), numbers.split("/")))
+    return HttpResponse(result)
