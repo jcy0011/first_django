7aba804e4c52a6b4c580baef94ffdf13089d2567
jcy0011 <jcy0011@yonsei.ac.kr>
2019-02-08 11:10:48 +0900

    static

--- first_django/settings.py
+++ first_django/settings.py
@@ -128,7 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'first_django', 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 INTERNAL_IPS = ['127.0.0.1']
 
