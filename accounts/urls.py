690f4501df87e960e5b5db54549fb754d75f9ce1
jcy0011 <jcy0011@yonsei.ac.kr>
2019-02-08 15:18:51 +0900

    dd

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,15 @@
+from django.conf import settings
+from django.conf.urls import url
 from django.urls import path, re_path
 from . import views
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
-    path('profile/', views.profile, name='profile'),
+    url(r'^login/$', auth_views.login, name='login',
+        kwargs={'template_name': 'accounts/login_form.html'}),
+    url(r'^logout/$', auth_views.logout, name='logout',
+        kwargs={'next_page': settings.LOGIN_URL}),
+    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^signup/$', views.signup, name='signup'),
 ]
